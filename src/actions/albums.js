690a107d2d7e5a8ff76cd1eb8fa621a1a654bd01
andela-fomokaro <faith.omokaro@andela.com>
2019-06-12 12:18:59 +0100

    write album action to get albums from 'https://jsonplaceholder.typicode.com/albums

---
+++ src/actions/albums.js
@@ -0,0 +1,11 @@
+import axios from 'axios'
+
+export const setAlbums = (albums) => ({
+ type: 'GET_ALBUMS',
+ albums
+});
+
+export const getAlbums = () => {
+ return dispatch => axios.get('https://jsonplaceholder.typicode.com/albums').then((res) => {
+   dispatch(setAlbums(res.data));
+})}; 
