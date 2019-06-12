2499dba2beec9e7b0e5e8537dc4015a3f8551f31
andela-fomokaro <faith.omokaro@andela.com>
2019-06-12 20:32:37 +0100

    setup test file for project write test for albums component

---
+++ src/tests/components/albums/album.test.js
@@ -0,0 +1,52 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import { AlbumPage } from '../../../components/album/AlbumPage';
+
+describe("AlbumPage", () => {
+  let props;
+  let wrapper;
+
+  beforeEach(() => {
+    props = {
+      users: [],
+      albums: [],
+      getUsers: jest.fn(),
+      getAlbums: jest.fn(),
+    };
+    wrapper = shallow(<AlbumPage {...props} />);
+  });
+
+  afterEach(() => { wrapper = null; });
+
+  
+  describe('renders', () => {
+    it('should exist', () => {
+      expect(wrapper.exists()).toBe(true);
+    });
+
+    it('should not have any regressions', () => {
+      expect(wrapper).toMatchSnapshot();
+    });
+
+    it('sets albums', () => {
+      wrapper.setState({
+        userId: 1,
+        title: 'lorem ipsum',
+        color: '#000fff',
+        userName: 'Omokaro Faith',
+        albumId: 2
+      });
+
+      const result = {
+        userId: 1,
+        title: 'lorem ipsum',
+        color: '#000fff',
+        userName: 'Omokaro Faith',
+        albumId: 2,
+        albums: [],
+      };
+
+      expect(wrapper.state()).toEqual(result);
+    });
+  });
+})
