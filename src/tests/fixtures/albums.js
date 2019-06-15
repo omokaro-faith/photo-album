30c6f96b8806877df11a0410428b381661510012
andela-fomokaro <faith.omokaro@andela.com>
2019-06-15 08:23:01 +0100

    add fixtures to mocking users and album data

---
+++ src/tests/fixtures/albums.js
@@ -0,0 +1,7 @@
+export default [
+  {id: 1, title: 'This fall apart'},
+  {id: 2, name: 'Bisi goes to school'},
+  {id: 3, name: 'The church hill'},
+  {id: 4, name: 'dauty'},
+  {id: 5, name: 'kelly rowrow'}
+];
---
+++ src/tests/fixtures/users.js
@@ -0,0 +1,7 @@
+export default [
+  {id: 1, name: 'Leanne Graham'},
+  {id: 2, name: 'Ervin Howell'},
+  {id: 3, name: 'Teni askamaya'},
+  {id: 4, name: 'dauty'},
+  {id: 5, name: 'kelly rowrow'}
+];
