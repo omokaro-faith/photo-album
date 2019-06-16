7cdaa7c17cda95b527909e3006c1d2293fde413b
andela-fomokaro <faith.omokaro@andela.com>
2019-06-16 11:31:31 +0100

    remove reduntant import

--- src/routes/AppRoutes.js
+++ src/routes/AppRoutes.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
- import Header from '../components/header/Header';
 import AlbumPage from '../components/album/AlbumPage';
 import PhotoPage from '../components/photo/PhotoPage';
 import NotFoundPage from '../components/notfound/NotFoundPage';
@@ -10,10 +9,9 @@ import NotFoundPage from '../components/notfound/NotFoundPage';
  const AppRoute = () => (
   <BrowserRouter>
   <div>
-    <Header />
     <Switch>
-      <Route path="/" component={AlbumPage} exact={true}/>
-      <Route path="/photo-page" component={PhotoPage}/>
+      <Route path='/' component={AlbumPage} exact={true}/>
+      <Route path='/photo-page/:albumId/:albumTitle/:albumOwner' component={PhotoPage}/>
       <Route component={NotFoundPage}/>
     </Switch>
   </div>
