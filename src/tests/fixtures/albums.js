ad52c1a9e76d93d3236068a92b5304c41a2ca1b5
andela-fomokaro <faith.omokaro@andela.com>
2019-06-16 11:30:11 +0100

    update album fixture files

--- src/tests/fixtures/albums.js
+++ src/tests/fixtures/albums.js
@@ -1,7 +1,7 @@
 export default [
-  {id: 1, title: 'This fall apart'},
-  {id: 2, name: 'Bisi goes to school'},
-  {id: 3, name: 'The church hill'},
-  {id: 4, name: 'dauty'},
-  {id: 5, name: 'kelly rowrow'}
+  {userId: 1, title: 'This fall apart', id: 1},
+  {userId: 2, title: 'Bisi goes to school', id: 2},
+  {userId: 3, title: 'The church hill', id: 3},
+  {userId: 4, title: 'dauty', id: 4},
+  {userId: 5, title: 'kelly rowrow', id: 5}
 ];
