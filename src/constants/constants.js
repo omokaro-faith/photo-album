8e268895ec46864de957553771c0128aa4511f58
andela-fomokaro <faith.omokaro@andela.com>
2019-06-16 11:32:22 +0100

    update constants

---
+++ src/constants/constants.js
@@ -0,0 +1,45 @@
+export const DEFAULT_COLOR_PALLETTE = 
+  [
+    {
+    userId: 1,
+    color: "CC6699",
+    },
+    {
+      userId: 2,
+      color: "F1B02F",
+    },
+    {
+      userId: 3,
+      color: "EADE84",
+    },
+    {
+      userId: 4,
+      color: "A3D064",
+      },
+      {
+        userId: 5,
+        color: "11B2AA",
+      },
+      {
+        userId: 6,
+        color: "177C99",
+      },
+      {
+        userId: 7,
+        color: "CCCCFF",
+      },
+      {
+        userId: 8,
+        color: "993333",
+      },
+      {
+        userId: 9,
+        color: "669999",
+      },
+      {
+        userId: 10,
+        color: "FF9999",
+      },
+ ];
+
+export const ITEMS_PER_PAGE = 10;
