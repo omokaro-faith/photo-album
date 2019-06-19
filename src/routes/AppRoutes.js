b40ee8c28f2d7b7835a99fe7851cac521b040fe9
andela-fomokaro <faith.omokaro@andela.com>
2019-06-19 12:02:30 +0100

    add loader fallback to suspense, add page logo

--- src/routes/AppRoutes.js
+++ src/routes/AppRoutes.js
@@ -1,19 +1,21 @@
 import React, { lazy, Suspense } from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
+const Logo = lazy(() => import('../components/header/Logo'));
 
 const AlbumPage = lazy(() => import('../components/album/AlbumPage'));
 
 const PhotoPage = lazy(() => import('../components/photo/PhotoPage'));
 
-const NotFoundPage = lazy(() => import('../components/photo/PhotoPage'));
+const NotFoundPage = lazy(() => import('../components/notfound/NotFoundPage'));
 
  const AppRoute = () => (
   <BrowserRouter>
-  <div>
-      <Suspense fallback={<div>Loading...</div>}>
+  <div className='wrapper__item'>
+      <Suspense fallback={<div className="loader"></div>}>
+      <Route component={Logo} exact/>
         <Switch>
-          <Route path='/' component={AlbumPage} exact={true}/>
+          <Route path='/' component={AlbumPage} exact/>
           <Route path='/photo-page/:albumId/:albumTitle/:albumOwner' component={PhotoPage}/>
           <Route component={NotFoundPage}/>
         </Switch>
